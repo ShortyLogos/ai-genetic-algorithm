9690af3f47348ccea9a55ad0024b47acae864a77
Déric Marchand <dericmarchand@gmail.com>
2022-05-13 11:50:34 -0400

    tweak on open box problem

--- model/geometry_optimization.py
+++ model/geometry_optimization.py
@@ -1,5 +1,10 @@
+import random
+
 import numpy as np
 import model.gacvm as gacvm
+from PySide6.QtCore import Qt, Slot, Signal, QSize, QPointF, QRectF
+from PySide6.QtGui import QPolygonF, QTransform
+from PySide6 import *
 import model.umath as umath
 import sys
 
@@ -29,10 +34,10 @@ import sys
 # Forme normalisée -> 0 à 1 de chaque côté
 
 class GeometryOptimizationProblem:
-    def __init__(self, polygon, obstacles, space_rectangle):
+    def __init__(self, surface, polygon, obstacles_count):
         self.__polygon = polygon # QPolygonF
-        self.__obstacles = obstacles # Liste de QPointF
-        self.__space_rectangle = space_rectangle # QRectangleF
+        self.__surface = surface # QRectangleF
+        self.__obstacles_count = obstacles_count
         self.__translationX_range = np.array([-500, 500]) # -width à width du QRectangleF
         self.__translationY_range = np.array([-500, 500]) # -height à height du QRectangleF
         self.__rotation_range = np.array([0, 360])
@@ -42,35 +47,60 @@ class GeometryOptimizationProblem:
                                        [self.__rotation_range],
                                        [self.__scaling_range]),
                                        ('tx', 'ty', 'r', 's'))
+        self.__obstacles = []
+        self.transform = QTransform()
+        self.generate_obstacles()
+
 
     @property
-    def box_width(self):
-        return self.__box_width
+    def surface(self):
+        return self.__surface
 
-    @box_width.setter
-    def box_width(self, value):
-        self.__box_width = value
-
-    @property
-    def box_height(self):
-        return self.__box_height
-
-    @box_height.setter
-    def box_height(self, value):
-        self.__box_height = value
+    @surface.setter
+    def surface(self, new_surface):
+        self.__surface = new_surface
 
     @property
-    def corner_range(self):
-        return self.__corner_range
+    def polygon(self):
+        return self.__polygon
+
+    @polygon.setter
+    def polygon(self, new_polygon):
+        self.__polygon = new_polygon
+
+    @property
+    def obstacles_count(self):
+        return self.__obstacles_count
+
+    @obstacles_count.setter
+    def obstacles_count(self, value):
+        self.__obstacles_count = value
 
     @property
     def domains(self):
         return self.__domains
 
+    def generate_obstacles(self):
+        self.__obstacles.clear()
+        for _ in range(self.__obstacles_count):
+            p = QPointF()
+            p.setX(p.x() + random.uniform(0, self.surface.width))
+            p.setY(p.y() + random.uniform(0, self.surface.height))
+            self.__obstacles.append(p)
+
     # fitness
-    def __call__(self, c):
-        corner = c[0]
-        return ((self.__box_height - (2 * corner)) * (self.__box_width - (2 * corner))) * corner
+    def __call__(self, dimensions):
+        # corner = c[0]
+        # return ((self.__box_height - (2 * corner)) * (self.__box_width - (2 * corner))) * corner
+        self.transform.translate(dimensions[0], dimensions[1])
+        self.transform.rotate(dimensions[2])
+        self.transform.scale(dimensions[3], dimensions[3])
+        self.transform.map(self.polygon)
+        # 1. transformation
+        # 2. forme.checkPoints(point)
+        # 3. canvas.intersects(forme)
+        # IF 2 et 3 FALSE alors return AIRE
+        # SINON return 0
 
 
 if __name__ == '__main__':
