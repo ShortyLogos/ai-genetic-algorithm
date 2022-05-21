bebd0c8a79d3ab443c0ed4ea3b0a6f88bfcb0c61
Déric Marchand <dericmarchand@gmail.com>
2022-05-21 19:02:53 -0400

    custom mutation strategy complete!

--- model/custom_strategies.py
+++ model/custom_strategies.py
@@ -1,27 +1,30 @@
 from model.gacvm import MutationStrategy
 import numpy as np
 
-# pour appliquer la stratégie :
-# une instance de ga a une variable parameters, qui est un objet
-# ex : ga.parameters pour obtenir l'instance
-# ga.instance_de_parameters.mutation_strategy = instance_strategie_mutation_personnalisee
-# instance_de_parameters.mutation_strategy = instance_strategie_mutation_personnalisee
+# ----------- WILD GENES MUTATION STRATEGY ----------------
+# Permet d'appliquer une mutation sur plusieurs gènes à la fois.
+# La probabilité d'affecter un gène donné diminue avec le nombre de gènes
+# qui composent le chromosome.
 
-class DoubleGenesStrategy(MutationStrategy):
+# Ainsi, les entités plus complexes (chromosome étendu) subissent des changements moins
+# prévisibles mais plus mesurés la plupart du temps, à l'image du monde naturel.
+# Les entités plus simples sont quant à elles appelées à évoluer plus rapidement.
+
+class WildGenesStrategy(MutationStrategy):
     def __init__(self):
         super().__init__()
 
     @staticmethod
     def name():
-        return 'Slalom Style Mutation'
+        return 'Wild Genes Strategy'
 
     def mutate(self, offsprings, mutation_rate, domains):
         def do_mutation(offspring, mutation_rate, domains):
             if self._rng.random() <= mutation_rate:
-                index = self._rng.integers(0, offsprings.shape[1]) # de 0 au nbr de domaine de l'enfant
-                offspring[index] = domains.random_value(index)
-                offspring[0] = domains.random_value(0)
-                offspring[1] = domains.random_value(1)
+                for i in range(offsprings.shape[1]):
+                    if self._rng.random() <= 1 / offsprings.shape[1]:
+                        offspring[i] = domains.random_value(i)
 
         np.apply_along_axis(do_mutation, 1, offsprings, mutation_rate, domains)
+
 
