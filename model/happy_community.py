4a65652c121c5f79bd753fa9b4abf961883562ae
Déric Marchand <dericmarchand@gmail.com>
2022-05-22 13:43:01 -0400

    work on Excel + start coding Happy Problem

--- model/happy_community.py
+++ model/happy_community.py
@@ -11,13 +11,15 @@ class HappyCommunityProblem:
     def __init__(self, community_context, community_size=200):
         self.__community_context = community_context
         self.__community_size = community_size
+        self.__jobs_value = None
+        self.generate_jobs()
         self.__priorities = None  # array des coefficients de pondération selon le contexte
         # méthode generate_priorities(community_context) ?
+        self.generate_domain()
         self.__domains = gacvm.Domains(np.array([
             0
         ], float),
             ('doctor', 'engineer', 'farmer', 'worker'))
-        self.generate_jobs()
 
     @property
     def community_context(self):
@@ -27,10 +29,23 @@ class HappyCommunityProblem:
     def community_context(self, new_community):
         self.__community_context = new_community
 
-    def __call__(self, jobs):
-        pass
-
     def generate_jobs(self):
+        """
+        [0] -> Community Cost,
+        [1] -> Food Production,
+        [2] -> Goods Production,
+        [3] -> Health
+        """
+        self.__jobs_value = np.array(
+            [[0.5], [0.], [0.], [0.8]],  # Doctor
+            [[0.375], [0.15], [0.35], [0.125]],  # Engineer
+            [[0.1], [0.6], [0.], [0.05]],  # Farmer
+            [[0.025], [0.25], [0.65], [0.025]],  # Worker
+            dtype=float)
+
+    def generate_domain(self):
+
+    def __call__(self, jobs):
         pass
 
 class SocioPoliticalContext:
@@ -103,9 +118,9 @@ class CommunityContext:
     def __init__(self, socio_political_context):
         # Les indices sont calculé de 0 à 10
         # On doit faire appel à des setters pour les paramétrer
-        self.__religious_sentiment = 0
-        self.__domestic_stability = 0
-        self.__education_rate = 0
+        self.__religious_sentiment = 10
+        self.__domestic_stability = 10
+        self.__education_rate = 1
         # Ci-dessous, les priorités d'une communauté (moyenne pondérée dont la somme = 1)
         self.__community_cost = 0
         self.__health = 0
