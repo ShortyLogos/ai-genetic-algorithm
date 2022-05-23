02df7bcd7d304b1b9197196145fe7cc43294c73f
Karlipouette <karl-marchand@hotmail.com>
2022-05-23 17:19:35 -0400

    Vue final probleme 3

--- community_vue.py
+++ community_vue.py
@@ -8,6 +8,7 @@
 import math
 import gaapp
 from model.gacvm import Parameters, ProblemDefinition
+from model.happy_community import HappyCommunityProblem
 
 from PySide6.QtCore import Qt, Signal, Slot, QSignalBlocker
 from PySide6.QtWidgets import QHBoxLayout, QVBoxLayout, QWidget, QGroupBox, QFormLayout, QComboBox, QCheckBox
@@ -26,43 +27,60 @@ class CommunityPanel(gaapp.QSolutionToSolvePanel):
         super().__init__(parent)
         self._main_layout = QHBoxLayout()
         self.set_layout(self._main_layout)
-        #self._problem = modeleProblem()        ######  Size et autres différents  ######
+        self._problem = HappyCommunityProblem()
 
         #### Parameters ####
         paramsBox = QGroupBox("Parameters")
         paramsBox_layout = QVBoxLayout(paramsBox)
+        
         form_widget = QWidget()
         paramsForm_layout = QFormLayout(form_widget)   
         
-        min_commu_size = 1 #self._problem.min_commu_size
-        max_commu_size = 100 #self._problem.max_commu_size
-        initial = math.ceil((max_commu_size-min_commu_size)/2)
+        self._context_dropdown = QComboBox()
+        context_list = self._problem.context.preset_gui_contexts
+        for key in context_list.keys():
+            self._context_dropdown.add_item(key, context_list[key])
+        self._context_dropdown.add_item("Custom")
+        self._context_dropdown.currentIndexChanged.connect(self.__context_changed)
+        paramsForm_layout.add_row('Context', self._context_dropdown)
+
+        min_commu_size = self._problem.community_min_size
+        max_commu_size = self._problem.community_max_size
+        initial = self._problem.community_size
         self._size_widget, size_layout = create_scroll_int_value(min_commu_size, initial, max_commu_size)
         self._size_widget.valueChanged.connect(self.parameter_changed)
         paramsForm_layout.add_row('Community Size', size_layout)
 
-        self._context_dropdown = QComboBox()
-        """
-        for context in self._problem.context:
-            self._context_dropdown.add_item(context.name, context.data)
-        """
-        self._context_dropdown.add_item("Custom")
-        self._context_dropdown.currentIndexChanged.connect(self.__context_changed)
-        paramsForm_layout.add_row('Context', self._context_dropdown)
+        min_life_expectancy = self._problem.context.socio_political_context.life_expectancy_min
+        max_life_expectancy = self._problem.context.socio_political_context.life_expectancy_max
+        initial = self._problem.context.socio_political_context.life_expectancy
+        self._life_widget, life_layout = create_scroll_int_value(min_life_expectancy, initial, max_life_expectancy)
+        self._life_widget.valueChanged.connect(self.apply_custom)
+        paramsForm_layout.add_row('Life Expectancy', life_layout)
+ 
+        min_scroll = self._problem.context.min_trait_value
+        max_scroll = self._problem.context.max_trait_value
+        initial = self._problem.context.default_trait_value
         
-        criterias = {
-            'Pauvre': True,
-            'Malade': True,
-            'Egalitarian': False,
-            'Violent': True,
-            'Radioactif': True,
-            'Monarchie': True
-        } #self._problem.context.events
-        
+        self._religious_widget, religious_layout = create_scroll_int_value(min_scroll, initial, max_scroll)
+        self._religious_widget.valueChanged.connect(self.apply_custom)
+        paramsForm_layout.add_row('Religious Sentiment', religious_layout)
+        self._domestic_widget, domestic_layout = create_scroll_int_value(min_scroll, initial, max_scroll)
+        self._domestic_widget.valueChanged.connect(self.apply_custom)
+        paramsForm_layout.add_row('Domestic Stability', domestic_layout)
+        self._education_widget, education_layout = create_scroll_int_value(min_scroll, initial, max_scroll)
+        self._education_widget.valueChanged.connect(self.apply_custom)
+        paramsForm_layout.add_row('Education Rate', education_layout)
+
         paramsBox_layout.add_widget(form_widget)
 
+        self.__checkbox_list = []
+        criterias = self._problem.context.events
         for key in criterias.keys():
-            paramsBox_layout.add_widget(QCheckBox(key, self))
+            checkbox = QCheckBox(key, self)
+            checkbox.toggled.connect(self.apply_custom)
+            self.__checkbox_list.append(checkbox)
+            paramsBox_layout.add_widget(checkbox)
         
         paramsBox_layout.add_stretch()
 
@@ -70,8 +88,7 @@ class CommunityPanel(gaapp.QSolutionToSolvePanel):
         visuBox = QGroupBox("Visualization")
         visuBox_layout = QHBoxLayout(visuBox)
         self._image_visualization = QSimpleImage()
-        categories = None #self._problem.jobs
-        self._graphic_generator = StackedBarWidget(categories, 700, 300)
+        self._graphic_generator = StackedBarWidget(self._problem.jobs_tags, 700, 300)
         visuBox_layout.add_widget(self._image_visualization)
 
         #### General layout and connections ####
@@ -86,9 +103,12 @@ class CommunityPanel(gaapp.QSolutionToSolvePanel):
         custom_position = self._context_dropdown.count -1
         if choice != -1 and choice != custom_position:
             context = self._context_dropdown.item_data(choice)
-            #self._custom2_widget.set_value(context[0])
-            #self._custom1_widget.set_real_value(context[1])
-            # ...
+            self._life_widget.set_value(context[0])
+            self._religious_widget.set_value(context[1])
+            self._domestic_widget.set_value(context[2])
+            self._education_widget.set_value(context[3])
+            for i in range(len(self.__checkbox_list)):
+                self.__checkbox_list[i].set_checked(context[i+4])
         self._update_problem()
         signal_blocker.unblock()
 
@@ -99,9 +119,13 @@ class CommunityPanel(gaapp.QSolutionToSolvePanel):
 
     @Slot()
     def _update_problem(self):
-        #self._problem.community_size = self._size_widget.value
-        # ....
-        pass 
+        self._problem.context.community_size = self._size_widget.value
+        self._problem.context.socio_political_context.life_expectancy = self._life_widget.value
+        self._problem.context.set_community_trait(self._religious_widget.text, self._religious_widget.value)
+        self._problem.context.set_community_trait(self._domestic_widget.text, self._domestic_widget.value)
+        self._problem.context.set_community_trait(self._education_widget.text, self._education_widget.value)
+        for checkbox in self.__checkbox_list:
+            self._problem.context.socio_political_context.set_event(checkbox.text, checkbox.isChecked()) 
 
     @property
     def name(self):
@@ -120,7 +144,7 @@ class CommunityPanel(gaapp.QSolutionToSolvePanel):
         '''
         Retourne un objet complet de définition du problème. L'objet retourné est celui qui sera résoud par l'algorithme génétique.
         '''
-        return ProblemDefinition()
+        return ProblemDefinition(self._problem.domains, self._problem)
 
     @property
     def default_parameters(self):
