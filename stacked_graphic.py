5915215413c2b82c6a11637a5adf15545b0af0f3
golfjob565 <golfjob565@gmail.com>
2022-05-23 14:15:02 -0400

    commentaire

--- stacked_graphic.py
+++ stacked_graphic.py
@@ -1,5 +1,12 @@
+######### Sommaire: Ce fichier dessine le graphique pour notre troisième problème(happy_community)
+######### Membres:
+## Jean-Christophe Caron
+## Samuel Horvath
+## Déric Marchand
+## Karl Robillard Marchand
+######### Date de création:23/05/2022
+######### Référence: https://matplotlib.org/stable/gallery/lines_bars_and_markers/horizontal_barchart_distribution.html
 import numpy as np
-
 from PySide6 import QtCore, QtGui
 from PySide6.QtCore import Slot
 from PySide6.QtGui import QImage
@@ -17,11 +24,9 @@ class StackedBarWidget():
         self._jobs = categories
         self._img = None 
         self.update_graphic()
-
     @property
     def image(self):
         return self._img
-
     @Slot()
     def update_graphic(self, ga=None):
         if ga: {
@@ -38,16 +43,13 @@ class StackedBarWidget():
 
         labels = list(results.keys())
         data = np.array(list(results.values()))
-        
         data_cumulated = data.cumsum(axis=1)
         category_colors = colormaps['tab20'](
             np.linspace(0.15, 0.85, data.shape[1]))
-        
         dpi = 80
         figure = Figure(figsize=(self._width / dpi, self._height / dpi), dpi=dpi)
         canvas = FigureCanvas(figure)
         ax = figure.add_subplot(111)
-        
         for i, (colname, color) in enumerate(zip(self._jobs, category_colors)):
             widths = data[:, i]
             starts = data_cumulated[:, i] - widths
@@ -59,7 +61,6 @@ class StackedBarWidget():
         
         ax.legend(ncol=int(len(self._jobs)/2), bbox_to_anchor=(-0.1, 1),
             loc=3, fontsize=7.5)
-        
         canvas.draw()
         w, h = canvas.get_width_height()
         self._img = QImage(canvas.buffer_rgba(), w, h, w * 4, QImage.Format_ARGB32)
